b7e60aec70ae2b11b301abfb64ee6b1c2864faf6
yangxia605 <yangxia605@gmail.com>
2018-02-26 13:24:02 +0800

    Dynamically Generate Game Boards

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,11 +1,40 @@
-const blankLine = ' | | ';
-console.log('This is what an empty board would look like:');
-console.log(blankLine);
-console.log(blankLine);
-console.log(blankLine);
-const guessLine = '1 | | ';
-const bombLine = ' | B | ';
-console.log('This is what a board with a guess and a bomb on it would look like:')
-console.log(guessLine);
-console.log(bombLine);
-console.log(blankLine);
+const generatePlayerBoard = (numberOfRows,numberOfColumns) => {
+  let board = [];
+  for(let rowIndex = 0;rowIndex<numberOfRows;rowIndex++){
+    let row = [];
+    for(let columnIndex = 0;columnIndex<numberOfColumns;columnIndex++){
+      row.push(' ');
+    }
+    board.push(row);
+  }
+    return board;
+}
+
+const generateBombBoard = (numberOfRows,numberOfColumns,numberOfBombs) => {
+  let board = [];
+  for(let rowIndex = 0;rowIndex<numberOfRows;rowIndex++){
+    let row = [];
+    for(let columnIndex = 0;columnIndex<numberOfColumns;columnIndex++){
+      row.push(null);
+    }
+    board.push(row);
+  }
+  let numberOfBombsPlaced = 0;
+  while (numberOfBombsPlaced < numberOfBombs) {
+    let randomRowIndex  = Math.floor(Math.random() * numberOfRows);
+    let randomColunmIndex  = Math.floor(Math.random() * numberOfColumns);
+    board[randomRowIndex][randomColunmIndex] = 'B';
+    numberOfBombsPlaced++;
+  }
+    return board;
+}
+const printBoard = board => {
+  console.log(board.map( row => row.join(' | ')).join('\n'));
+};
+
+let playerBorad = generatePlayerBoard(3,3);
+let bombBorad = generateBombBoard(3,3,3);
+console.log('Player Borad: ');
+printBoard(playerBorad);
+console.log('Bomb Borad: ');
+printBoard(bombBorad);
