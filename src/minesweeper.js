0cd047e8fabab43be290fa831ddad104f49218e2
yangxia605 <yangxia605@gmail.com>
2018-02-27 23:01:26 +0800

    Add Player Interactivity

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -23,10 +23,39 @@ const generateBombBoard = (numberOfRows,numberOfColumns,numberOfBombs) => {
   while (numberOfBombsPlaced < numberOfBombs) {
     let randomRowIndex  = Math.floor(Math.random() * numberOfRows);
     let randomColunmIndex  = Math.floor(Math.random() * numberOfColumns);
-    board[randomRowIndex][randomColunmIndex] = 'B';
-    numberOfBombsPlaced++;
+    if(board[randomRowIndex][randomColunmIndex] !== 'B'){
+      board[randomRowIndex][randomColunmIndex] = 'B';
+      numberOfBombsPlaced++;
+    }
   }
     return board;
+}
+
+const getNumberOfNeighborBombs = (bombBorad, rowIndex, columnIndex) => {
+  const neighborOffsets = [[-1,-1],[-1,0],[-1,1],[0,-1],[1,1],[0,1],[1,-1],[1,0]];
+  const numberOfRows = bombBorad.length;
+  const numberOfColumns = bombBorad[0].length;
+  let numberOfBombs = 0;
+  neighborOffsets.forEach(offset => {
+    const neighborRowIndex = rowIndex+offset[0];
+    const neighborColumnIndex = columnIndex+offset[1];
+    if(neighborRowIndex >=0 && neighborRowIndex <= numberOfRows
+      && neighborColumnIndex >=0 && neighborColumnIndex <= numberOfColumns){
+      if(bombBorad[neighborRowIndex][neighborColumnIndex] === 'B'){
+        numberOfBombs++;
+      }
+    }
+  })
+  return numberOfBombs;
+}
+const flipTile = (playerBorad, bombBorad, rowIndex, columnIndex) => {
+  if(playerBorad[rowIndex][columnIndex] !== ' '){
+    return 'This tile has already been flipped!';
+  } else if(bombBorad[rowIndex][columnIndex] === 'B'){
+    playerBorad[rowIndex][columnIndex] = 'B';
+  }else {
+    playerBorad[rowIndex][columnIndex] = getNumberOfNeighborBombs(bombBorad, rowIndex, columnIndex)
+  }
 }
 const printBoard = board => {
   console.log(board.map( row => row.join(' | ')).join('\n'));
@@ -38,3 +67,6 @@ console.log('Player Borad: ');
 printBoard(playerBorad);
 console.log('Bomb Borad: ');
 printBoard(bombBorad);
+flipTile(playerBorad, bombBorad, 0, 0);
+console.log('Update Player Borad:');
+printBoard(playerBorad);
